3eef220efc58c6ee7a50c3a8918add670149bde4
Amanda Souza <amanda_souza@live.com>
2022-06-23 17:14:04 -0300

    layout area de video com VimeJS, componente Router

--- event-platform/src/pages/Event.tsx
+++ event-platform/src/pages/Event.tsx
@@ -1,13 +1,15 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
 import { Video } from "../components/Video";
 
 export function Event(){
+    const {slug} = useParams<{slug: string}>()
     return(
         <div className="flex flex-col">
             <Header />
             <main className="flex flex-1 min-h-screen">
-                <Video />
+                { slug ? <Video  lessonSlug={slug} /> : < div className="flex-1" />}
                 <Sidebar />
             </main>
         
